096add9fbdb04692e13acb37877491a49866edeb
Hitesh Tare <hiteshtare@gmail.com>
2021-06-18 17:30:13 +0530

    Input forms with validations using slot

--- actions/actions.py
+++ actions/actions.py
@@ -7,13 +7,25 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List, Union
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 #
 #
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
 
 
 class ActionHelloWorld(FormAction):
@@ -26,6 +38,41 @@ class ActionHelloWorld(FormAction):
         # A list of required slots that form has to be filled
         print("required_slots(tracker: Tracker)")
         return ["name", "ssn", "subject"]
+
+    @staticmethod
+    def subject_db() -> List[Text]:
+        return [
+            "it",
+            "space",
+            "computers",
+            "neurology",
+            "datascience",
+        ]
+
+    def validate_subject(self, value: Text, dispatcher: CollectingDispatcher,
+                         tracker: Tracker,
+                         domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        if value.lower() in self.subject_db():
+            return {"subject": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_subject")
+            return {"subject": None}
+
+    def validate_ssn(self, value: Text, dispatcher: CollectingDispatcher,
+                     tracker: Tracker,
+                     domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        if len(value) == 6:
+            return {"ssn": value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_ssn")
+            return {"ssn": None}
+
+        # def slots_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        #     return {
+        #         "subject_code": [
+        #             self.from_entity(entity="subject", intent="subject_entry")
+        #         ]
+        #     }
 
     def submit(self, dispatcher: CollectingDispatcher,
                tracker: Tracker,
