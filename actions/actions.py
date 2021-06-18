ea9cca75fa836f5fcb2c7e77819280c1080f973e
Hitesh Tare <hiteshtare@gmail.com>
2021-06-18 17:36:35 +0530

    comments

--- actions/actions.py
+++ actions/actions.py
@@ -30,15 +30,18 @@ from rasa_sdk.forms import FormAction
 
 class ActionHelloWorld(FormAction):
 
+    # ActionName name registed in domain
     def name(self) -> Text:
         return "admission_form"
 
+    # Invoked automatically
     @staticmethod
     def required_slots(tracker: Tracker) -> List[Text]:
         # A list of required slots that form has to be filled
         print("required_slots(tracker: Tracker)")
         return ["name", "ssn", "subject"]
 
+    # Invoked automatically
     @staticmethod
     def subject_db() -> List[Text]:
         return [
@@ -49,21 +52,28 @@ class ActionHelloWorld(FormAction):
             "datascience",
         ]
 
+    # To validate subject using syntax validate_slotname
     def validate_subject(self, value: Text, dispatcher: CollectingDispatcher,
                          tracker: Tracker,
                          domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        # self is same as this in Typescript
         if value.lower() in self.subject_db():
+            # slotname in string format
             return {"subject": value}
         else:
+            # to invoke response defined in domain use template
             dispatcher.utter_message(template="utter_wrong_subject")
             return {"subject": None}
 
+    # To validate ssn using syntax validate_slotname
     def validate_ssn(self, value: Text, dispatcher: CollectingDispatcher,
                      tracker: Tracker,
                      domain: Dict[Text, Any]) -> Dict[Text, Any]:
         if len(value) == 6:
+            # slotname in string format
             return {"ssn": value}
         else:
+            # to invoke response defined in domain use template
             dispatcher.utter_message(template="utter_wrong_ssn")
             return {"ssn": None}
 
@@ -78,6 +88,7 @@ class ActionHelloWorld(FormAction):
                tracker: Tracker,
                domain: Dict[Text, Any]) -> List[Dict]:
 
+        # to invoke response defined in domain use template
         dispatcher.utter_message(template="utter_submit")
 
         return []
